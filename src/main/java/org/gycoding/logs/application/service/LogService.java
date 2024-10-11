0e67d67684322577f4c1d4be1683ed0b3cedc17e
gy-toxyc <ivi1000vm@gmail.com>
2024-10-11 13:26:07 +0200

    fix: null properties

--- src/main/java/org/gycoding/logs/application/service/LogService.java
+++ src/main/java/org/gycoding/logs/application/service/LogService.java
@@ -1,5 +1,6 @@
 package org.gycoding.logs.application.service;
 
+import jakarta.annotation.PostConstruct;
 import org.gycoding.logs.model.LogEntity;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Value;
@@ -9,21 +10,28 @@ import org.springframework.stereotype.Service;
 @Service
 public class LogService {
     private static MongoTemplate mongoTemplate;
+    private static String collectionName;
+    private static String origin;
 
     @Value("${gy.logs.collection.name}")
-    private static String collectionName;
+    private String collectionNameProp;
 
     @Value("${gy.logs.origin}")
-    private static String origin;
+    private String originProp;
 
     @Autowired
     public LogService(MongoTemplate mongoTemplate) {
         LogService.mongoTemplate = mongoTemplate;
     }
 
+    @PostConstruct
+    private void init() {
+        LogService.collectionName = this.collectionNameProp;
+        LogService.origin = this.originProp;
+    }
+
     public static void log(LogEntity logEntity) {
         logEntity.setOrigin(origin);
-
         System.out.println(logEntity.toString());
         mongoTemplate.save(logEntity.toJSON(), collectionName);
     }
