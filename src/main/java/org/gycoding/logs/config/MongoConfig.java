b58ab4abb02427b5bb29d6d8daf1d9b09386d99c
gy-toxyc <ivi1000vm@gmail.com>
2024-10-23 23:51:52 +0200

    fix: overriding database for logs in services

--- src/main/java/org/gycoding/logs/config/MongoConfig.java
+++ src/main/java/org/gycoding/logs/config/MongoConfig.java
@@ -7,9 +7,14 @@ import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.context.annotation.Profile;
 import org.springframework.data.mongodb.core.MongoTemplate;
+import org.springframework.data.mongodb.repository.config.EnableMongoRepositories;
 
 @Configuration
 @Profile("spring-logs")
+@EnableMongoRepositories(
+        basePackages = "org.gycoding.logs.application.service",
+        mongoTemplateRef = "logsMongoTemplate"
+)
 public class MongoConfig {
 
     @Value("${spring.data.mongodb.uri}")
@@ -22,8 +27,8 @@ public class MongoConfig {
         return MongoClients.create(mongoUri);
     }
 
-    @Bean
-    public MongoTemplate mongoTemplate() {
+    @Bean(name = "logsMongoTemplate")
+    public MongoTemplate logsMongoTemplate() {
         return new MongoTemplate(mongoClient(), databaseName);
     }
 }
